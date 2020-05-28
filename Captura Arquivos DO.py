b1cc79f356de65335899194525d0398f602f0b4b
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2020-05-28 13:51:20 -0300

    Update Captura Arquivos DO.py

--- Captura Arquivos DO.py
+++ Captura Arquivos DO.py
@@ -1,23 +1,33 @@
-from bs4 import BeautifulSoup as bs
 from requests import get
+from requests import request
+import requests
+import os
+from pathlib import Path
+from bs4 import BeautifulSoup as bs
 
-#Guarda o link da página princial do DO em uma variável.
+r = requests.get('http://doweb.rio.rj.gov.br/apifront/portal/edicoes/ultimas_edicoes.json')
+itens = r.json()['itens'] #Captura todas as edições
+id_dia = itens[0]['id'] #Captura o id da ultima edição
+id_data = str(itens[0]['data']).replace('/', '-') #Captura a data da ultima edição
+#id_data_arquivo = str(id_data).replace('/','-')
+print(id_dia, id_data)
 
 base_url = 'http://doweb.rio.rj.gov.br'
-
-# Complementa o link principal com o link da publicação diária. O número ao final, no exemplo 4132, é o identificador do dia do DO".
-# Após acessar o link, capturo todos os "Identificadores" que são os números que identificam cada arquivo publicado no DO.
-
-
-url = f'{base_url}/portal/visualizacoes/view_html_diario/4132'
+url = f'{base_url}/portal/visualizacoes/view_html_diario/{id_dia}'
 page = bs(get(url).content, 'html.parser')
 identificadores = [x.text.split()[0] for x in page.find_all('a')]
 
-
-# Seleciona cada identificador, substitui no link respectivo e salva cada arquivo em um txt.
-
 conteudo = '{}/apifront/portal/edicoes/publicacoes_ver_conteudo/{}'
-for identificador in identificadores:
-    page = bs(get(conteudo.format(base_url, identificador)).text, 'html.parser')
-    with open(f'arquivos/09-05-2019/{identificador}.txt', 'w', encoding='utf-8') as f:
-        f.writelines([x.text.strip() + '\n' for x in page.find_all('p')])
+
+fileName = (f'D:/Projetos Python/Diario Oficial/{id_data}.txt')
+file = Path(fileName)
+if file.is_file():
+    pass
+else:
+    try:
+        for identificador in identificadores:
+            page = bs(get(conteudo.format(base_url, identificador)).text, 'html.parser')
+            with open(f'D:/Projetos Python/Diario Oficial/{id_data}.txt', 'a', encoding='utf-8') as f:
+                f.writelines([x.text.strip() + '\n' for x in page.find_all('p')])
+    except:
+        pass
